4107b3d6e016a326f5dcccfd8a1f297910197a15
Hugo Martinez <hsmartin@andrew.cmu.edu>
2024-09-14 02:10:13 -0400

    res

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ const port = 3000;
 
 app.use(cors()); // Enable CORS for all routes
 
-// const { scrapeImages } = require('./test_js');
-const { searchPimEyes } = require('./pimeyes.js');
+const { scrapeImages } = require('./test_js');
+// const { searchPimEyes } = require('./pimeyes.js');
 
 // API endpoint to scrape images
 app.get('/scrape-images', async (req, res) => {
     try {
-        const images = await searchPimEyes("C:/Users/hugom/Downloads/face_screenshot.png");
+        // const images = await searchPimEyes("C:/Users/hugom/Downloads/face_screenshot.png");
 
-        // const images = await scrapeImages();
+        const images = await scrapeImages();
         res.json({ success: true, images: images });
     } catch (error) {
         res.status(500).json({ success: false, message: error.message });
